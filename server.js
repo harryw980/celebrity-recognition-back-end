f32e58d61d283104a834dbbd3a5786790e47bfd3
Hanlei Wang <hanlei430@gmail.com>
2020-06-24 15:29:22 -0400

    added connection string for database

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const bcrypt = require('bcrypt-nodejs');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-cylindrical-08755',
-      user : 'postgres',
-      password : 'test',
-      database : 'face_recogntion'
+      connectString : process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
